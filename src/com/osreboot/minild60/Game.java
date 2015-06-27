eb717f0dc7d9fa41466460efd8dcdb9d6d5e2a72
Adam Krpan <creeperinatardis@gmail.com>
2015-06-27 14:49:39 -0700

    Loaded tilemaps into the game.

--- src/com/osreboot/minild60/Game.java
+++ src/com/osreboot/minild60/Game.java
@@ -1,13 +1,21 @@
 package com.osreboot.minild60;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 import org.newdawn.slick.Color;
 
+import com.osreboot.minild60.TextureManager.TextureSeries;
 import com.osreboot.minild60.backend.Player;
 import com.osreboot.ridhvl.HvlTextureUtil;
 import com.osreboot.ridhvl.painter.painter2d.HvlPainter2D;
+import com.osreboot.ridhvl.tile.HvlLayeredTileMap;
 
 public class Game {
 	public static float cameraX, cameraY;
+
+	private static HvlLayeredTileMap map;
 
 	private static Player player;
 
@@ -17,9 +25,32 @@ public class Game {
 
 	public static void initialize() {
 		reset();
+
+		try {
+			String path = "res/TestLevel.map";
+			StringBuilder sb = new StringBuilder();
+			BufferedReader read = new BufferedReader(new FileReader(path));
+
+			String line;
+			while ((line = read.readLine()) != null) {
+				sb.append(line);
+			}
+
+			read.close();
+			
+			map = HvlLayeredTileMap.load(sb.toString(),
+					TextureManager.getResource(TextureSeries.PLAY, 0), 0, 0,
+					Player.radius * 2, Player.radius * 2);
+		} catch (IOException e) {
+			e.printStackTrace();
+			return;
+		}
 	}
 
 	public static void update(float delta) {
+		map.setX(cameraX);
+		map.setY(cameraY);
+		map.draw(delta);
 		player.update(delta);
 		player.draw(delta);
 		HvlPainter2D.hvlDrawQuad(-16 + cameraX, 16 + cameraY, 32, 32,
--- src/com/osreboot/minild60/TextureManager.java
+++ src/com/osreboot/minild60/TextureManager.java
@@ -7,20 +7,25 @@ import com.osreboot.ridhvl.loader.HvlTextureLoader;
 public class TextureManager {
 
 	private static HvlTextureLoader uiLoader = new HvlTextureLoader(10);
+	private static HvlTextureLoader playLoader = new HvlTextureLoader(10);
 	
 	public enum TextureSeries{
-		UI
+		UI,
+		PLAY
 	}
 	
 	public static void initialize(){
 		uiLoader.loadResource("White");
 		uiLoader.loadResource("TempArrow");
 		uiLoader.loadResource("Font");
+		
+		playLoader.loadResource("TestTilesheet");
 	}
 	
 	public static Texture getResource(TextureSeries seriesArg, int indexArg){
 		switch(seriesArg){
 		case UI: return uiLoader.getResource(indexArg);
+		case PLAY: return playLoader.getResource(indexArg);
 		default: return null;
 		}
 	}
