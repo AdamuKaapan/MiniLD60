a6043d16e5c77cdfbed5cd6e71c0e0794376e5d5
Adam Krpan <creeperinatardis@gmail.com>
2015-06-27 11:45:26 -0700

    Initial commit (take 2)

---
+++ src/com/osreboot/minild60/Main.java
@@ -0,0 +1,26 @@
+package com.osreboot.minild60;
+
+import com.osreboot.ridhvl.display.collection.HvlDisplayModeDefault;
+import com.osreboot.ridhvl.template.HvlTemplate2DBasic;
+
+public class Main extends HvlTemplate2DBasic {
+
+	public static void main(String[] args) {
+		new Main();
+	}
+	
+	public Main()
+	{
+		super(60, 1280, 720, "Mini Ludum Dare 60 - Celebrate Ludum Dare", new HvlDisplayModeDefault());
+	}
+
+	@Override
+	public void initialize() {
+		
+	}
+
+	@Override
+	public void update(float arg0) {
+		
+	}
+}
