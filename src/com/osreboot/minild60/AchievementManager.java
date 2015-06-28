dee768a6af77f4235e17ac124ec2689f0a607e62
Adam Krpan <creeperinatardis@gmail.com>
2015-06-27 18:07:41 -0700

    Achievements now work!

---
+++ src/com/osreboot/minild60/AchievementManager.java
@@ -0,0 +1,90 @@
+package com.osreboot.minild60;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.newdawn.slick.opengl.Texture;
+
+import com.osreboot.ridhvl.config.HvlConfigIgnore;
+import com.osreboot.ridhvl.config.HvlConfigUtil;
+
+public class AchievementManager
+{
+	@HvlConfigIgnore
+	public static Texture[] icons;
+	
+	@HvlConfigIgnore
+	public static String[] titles, descriptions;
+	
+	public static boolean unlockeds[];
+	
+	public static void initialize()
+	{
+		icons = new Texture[0];
+		titles = new String[0];
+		descriptions = new String[0];
+		unlockeds = new boolean[0];
+		
+		addAchievement(new Achievement(null, "Hey!", "Meh", false));
+		
+		HvlConfigUtil.loadStaticConfig(AchievementManager.class, "res\\achievements");
+		
+		System.out.println(getAchievement("Hey!").isUnlocked());
+	}
+	
+	private static void addAchievement(Achievement acc)
+	{
+		resizeArrays(unlockeds.length + 1);
+		icons[icons.length - 1] = acc.getIcon();
+		titles[titles.length - 1] = acc.getTitle();
+		descriptions[titles.length - 1] = acc.getDescription();
+		unlockeds[unlockeds.length - 1] = acc.isUnlocked();
+	}
+	
+	public static Achievement getAchievement(String name)
+	{
+		for (int i = 0; i < titles.length; i++)
+		{
+			if (!titles[i].equals(name)) continue;
+			
+			return new Achievement(icons[i], titles[i], descriptions[i], unlockeds[i]);
+		}
+		
+		return null;
+	}
+	
+	public static void setUnlocked(String name, boolean val)
+	{
+		for (int i = 0; i < titles.length; i++)
+		{
+			if (!titles[i].equals(name)) continue;
+			
+			unlockeds[i] = val;
+		}
+	}
+	
+ 	private static void resizeArrays(int size)
+	{
+		Texture[] newIcons = new Texture[size];
+		for (int i = 0; i < icons.length; i++)
+			newIcons[i] = icons[i];
+		
+		String[] newTitles = new String[size];
+		for (int i = 0; i < titles.length; i++)
+			newTitles[i] = titles[i];
+		
+		String[] newDescs = new String[size];
+		for (int i = 0; i < descriptions.length; i++)
+			newDescs[i] = descriptions[i];
+		
+		boolean[] newUnlock = new boolean[size];
+		for (int i = 0; i < unlockeds.length; i++)
+			newUnlock[i] = unlockeds[i];
+		
+		icons = newIcons;
+		titles = newTitles;
+		descriptions = newDescs;
+		unlockeds = newUnlock;
+	}
+}
--- src/com/osreboot/minild60/Game.java
+++ src/com/osreboot/minild60/Game.java
@@ -4,12 +4,8 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
-import org.newdawn.slick.Color;
-
 import com.osreboot.minild60.TextureManager.TextureSeries;
 import com.osreboot.minild60.backend.Player;
-import com.osreboot.ridhvl.HvlTextureUtil;
-import com.osreboot.ridhvl.painter.painter2d.HvlPainter2D;
 import com.osreboot.ridhvl.tile.HvlLayeredTileMap;
 
 public class Game {
@@ -23,8 +19,10 @@ public class Game {
 		player = new Player();
 	}
 
-	public static void initialize() {		
+	public static void initialize() {
 		reset();
+		
+		AchievementManager.initialize();
 
 		try {
 			String path = "res/TestLevel.map";
@@ -37,7 +35,7 @@ public class Game {
 			}
 
 			read.close();
-			
+
 			map = HvlLayeredTileMap.load(sb.toString(),
 					TextureManager.getResource(TextureSeries.PLAY, 0), 0, 0,
 					Player.radius * 2, Player.radius * 2);
@@ -53,6 +51,5 @@ public class Game {
 		map.draw(delta);
 		player.update(delta);
 		player.draw(delta);
-		HvlPainter2D.hvlDrawQuad(cameraX + 256, cameraY + 256, map.getTileWidth(), map.getTileHeight(), HvlTextureUtil.getColoredRect((int)map.getTileWidth(), (int)map.getTileHeight(), Color.red));
 	}
 }
