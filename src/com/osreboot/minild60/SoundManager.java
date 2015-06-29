c86727d94b396b1eb37289074e74933cc7172e8b
osreboot <calvin.16.weaver@gmail.com>
2015-06-29 16:39:34 -0700

    Added sounds and things

--- src/com/osreboot/minild60/SoundManager.java
+++ src/com/osreboot/minild60/SoundManager.java
@@ -1,16 +1,18 @@
 package com.osreboot.minild60;
 
+import org.newdawn.slick.openal.Audio;
 import org.newdawn.slick.openal.SoundStore;
-import org.newdawn.slick.opengl.Texture;
 
-import com.osreboot.ridhvl.loader.HvlTextureLoader;
+import com.osreboot.ridhvl.loader.HvlSoundLoader;
 
 public class SoundManager {
 
-	private static HvlTextureLoader menuLoader = new HvlTextureLoader(10);
+	private static HvlSoundLoader menuLoader = new HvlSoundLoader(10);
+	private static HvlSoundLoader songLoader = new HvlSoundLoader(10);
 	
 	public enum SoundSeries{
-		MENU
+		MENU,
+		SONG
 	}
 	
 	public static void initialize(){
@@ -18,10 +20,19 @@ public class SoundManager {
 	}
 	
 	public static void update(){
+		if(OptionsConfig.volume == 0){
+			if(getResource(SoundSeries.SONG, 0).isPlaying()) getResource(SoundSeries.SONG, 0).stop();
+		}else{
+			if(!getResource(SoundSeries.SONG, 0).isPlaying()) getResource(SoundSeries.SONG, 0).playAsMusic(1, OptionsConfig.volume, false);
+		}
 		SoundStore.get().poll(0);
 	}
 	
-	public static Texture getResource(SoundSeries seriesArg, int indexArg){
+	public static void reset(){
+		if(getResource(SoundSeries.SONG, 0).isPlaying()) getResource(SoundSeries.SONG, 0).stop();
+	}
+	
+	public static Audio getResource(SoundSeries seriesArg, int indexArg){
 		switch(seriesArg){
 		case MENU: return menuLoader.getResource(indexArg);
 		default: return null;
